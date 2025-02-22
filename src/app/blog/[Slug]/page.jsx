51ef8fb20abb789c0f6d25673f977d92460ab37f
basnetkazi64@gmail.com <basnetkazi64@gmail.com>
2025-02-22 21:33:43 +0545

    responsive

--- src/app/blog/[Slug]/page.jsx
+++ src/app/blog/[Slug]/page.jsx
@@ -29,7 +29,7 @@ const SinglePage = () => {
 
 
   return (
-    <div className="min-h-[200px]">
+    <div className="pt-20 min-h-[95vh]  bg-slate-200">
       {loading ? (
         <div className="flex justify-center items-center h-40">
           <p className="text-lg text-gray-900">Loading post...</p>
--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,14 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 const SinglePost = ({ img, price, category, title, summary, slug }) => {
   return (
-    <div>
-        <Image src={img} width={100} height={100} alt="post image" unoptimized />
-        <p>{price}</p>
+    <div className="container max-w-[768px] ">
+        <Image className="max-w-[500px] w-[100%] " src={img} width={100} height={100} alt="post image" unoptimized />
+        <div className="flex- justify-between">
         <p>{category}</p>
-        <p>{title}</p>
-        <p>{summary}</p>
-        <p>{slug}</p>
+        <p className="font-bold">{price}</p>
+        </div>
+        <p className="text-lg font-bold ">{title}</p>
+        <p className="text-sm font-medium" >{summary}</p>
+        {/* <p>{slug}</p> */}
     </div>
   )
 }
